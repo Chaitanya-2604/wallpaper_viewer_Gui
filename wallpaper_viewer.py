7b06eb02441a6e0e2553a9a97042c49cb30da697
chaitanya_larokar <chaitanyalarokar9@gmail.com>
2023-12-26 18:44:02 +0530

    wallpaper_viewer

---
+++ wallpaper_viewer.py
@@ -0,0 +1,37 @@
+# import tkinter with all pakages
+from tkinter import *
+from PIL import Image,ImageTk
+import os
+
+
+def rotate_image():
+    global counter
+    #img_label.config(image=img_array[counter%len(img_array)])
+    #counter=counter+1
+    counter = (counter + 1) % len(img_array)  # Update the counter
+    img_label.config(image=img_array[counter])  # Update image in label
+
+
+counter=1
+root=Tk()
+root.title('Wallpaper Viewer')
+
+root.geometry('250x400')
+root.config(background='black')
+
+files=os.listdir('wallpapers')
+
+img_array=[]
+for file in files:
+    img=Image.open(os.path.join('wallpapers',file))
+    resized_img=img.resize((200,300))
+    img_array.append(ImageTk.PhotoImage(resized_img))
+
+img_label=Label(root,image=img_array[0])
+img_label.pack(pady=(15,10))
+
+next_btn=Button(root,text='Next',bg='white',fg='black',width=28,height=2,command=rotate_image)
+next_btn.pack()
+
+
+root.mainloop()
